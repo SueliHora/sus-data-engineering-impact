9ac71036945b1ddff34f36916303d8cd258421fc
Sueli da Hora <s.horamoreira@gmail.com>
2026-01-21 20:21:21 -0300

    feat: estrutura profissional de Analytics Engineer para o SUS

---
+++ projeto_sus/sus-data-engineering-impact/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analytics Engineer e IA para otimização de filas no SUS',
+    author='Sueli da Hora Moreira',
+    license='MIT',
+)
